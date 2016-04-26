7ed4e0eeffb44b6337044f20c3d9bb3febcdbf4e
PU Chengcheng <77202714@qq.com>
2016-04-26 20:00:14 +0800

    finished question 1

---
+++ Gliese0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gliese0
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TowSum t = new TowSum();
+
+			var intArr = new int[] { 3, 2, 1, 3, 4, 5 };
+			var target = 9;
+
+			PrintArray(t.TwoSum(intArr, target));
+			Console.ReadLine();
+		}
+
+		static void PrintArray(int[] array)
+		{
+			foreach (var i in array)
+				Console.Write(i + " ");
+		}
+	}
+}
---
+++ Gliese0/TowSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gliese0
+{
+	public class TowSum
+	{
+		public int[] TwoSum(int[] nums, int target)
+		{
+			var result = new int[2];
+			for (var i = 0; i < nums.Length - 1; i++)
+			{
+				for (var j = i + 1; j < nums.Length; j++)
+				{
+					if (nums[i] + nums[j] == target)
+					{
+						result[0] = i;
+						result[1] = j;
+						break;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
