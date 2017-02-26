ee382fae31b38f0b754cba4cfdcd1eb018e4c13d
HelloGalaxy <77202714@qq.com>
2017-02-26 15:06:20 +0800

    do q7 but  Run Code Status: Memory Limit Exceeded

--- Gliese0/Leetcode/Q7_ReverseInteger .cs
+++ Gliese0/Leetcode/Q7_ReverseInteger .cs
@@ -1,7 +1,64 @@
-﻿namespace Gliese0.Leetcode
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gliese0.Leetcode
 {
-    public class Q7_ReverseInteger 
+    // Reverse digits of an integer.
+    //   Example1: x = 123, return 321
+    //   Example2: x = -123, return -321
+    /// <summary>
+    /// 7. Reverse Integer
+    /// </summary>
+    public class Q7_ReverseInteger
     {
-         
+        private Stack<int> cache = new Stack<int>();
+
+        public int Reverse(int x)
+        {
+            try
+            {
+                var tmp = Math.Abs(x);
+                while (tmp > 0)
+                {
+                    if (tmp > 10)
+                    {
+                        var carry = tmp % 10;
+                        tmp = tmp / 10;
+                        cache.Push(carry);
+                    }
+                    else
+                    {
+                        cache.Push(tmp);
+                    }
+
+                }
+
+                tmp = 0;
+                var power = 0;
+                while (cache.Count > 0)
+                {
+                    var sum = cache.Pop() * Math.Pow(10, power);
+
+                    if (sum > int.MaxValue)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        tmp += (int)sum;
+                    }
+                    power++;
+                }
+                if (x < 0)
+                {
+                    tmp *= -1;
+                }
+                return tmp;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
     }
 }
--- Gliese0/Program.cs
+++ Gliese0/Program.cs
@@ -8,17 +8,20 @@ namespace Gliese0
     {
         static void Main(string[] args)
         {
-            var question = new Q2_Add2Nums();
+            var q7 = new Q7_ReverseInteger();
+            var input = 1563847412;
+            Console.Write(string.Format("input {0} the get  {1}", input, q7.Reverse(input)));
+            //var question = new Q2_Add2Nums();
 
-            var l1 = new ListNode(2);
-            l1.next = new ListNode(4);
-            l1.next.next = new ListNode(3);
+            //var l1 = new ListNode(2);
+            //l1.next = new ListNode(4);
+            //l1.next.next = new ListNode(3);
 
-            var l2 = new ListNode(5);
-            l2.next = new ListNode(6);
-            l2.next.next = new ListNode(4);
+            //var l2 = new ListNode(5);
+            //l2.next = new ListNode(6);
+            //l2.next.next = new ListNode(4);
 
-            PrintArray(question.AddTwoNumbers(l1, l2));
+            //PrintArray(question.AddTwoNumbers(l1, l2));
             Console.ReadLine();
         }
 
